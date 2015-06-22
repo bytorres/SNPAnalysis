33348f7ed9cd2a6c5c204ee59319ce526331c398
bytorres <bytorres@stanford.edu>
2015-06-22 12:58:28 -0700

    Plot selected snps, need to know the snp indx first (after running matlab). The uncolored dots are semi-transparent and the variance limit is very high, not very constricted.

--- AutomateSNPthrVar.R
+++ AutomateSNPthrVar.R
@@ -1,31 +1,29 @@
 
 t<-numeric(0)
-chosen_idx<-c(3,7,14,15,18,22,24)
-chosen_idx1<-c(29,30,12,35,49)
+
+choosen_idx<-c(11,22,76,99)
 
 #Making changes to see if its being recognized
 i=1
-for(n in 1:length(chosen_idx1))
+for(n in 1:length(choosen_idx))
 {
   n
-  a<-as.data.frame(table(nksnp_data[,chosen_idx1[n]])) # Tally on # of 0,1,2
-  NKG7_nm<-NKG7[which(as.numeric(nksnp_data[,chosen_idx1[n]])==2)]
-  RBC_nm<-RBC[which(as.numeric(nksnp_data[,chosen_idx1[n]])==2)]
+  a<-as.data.frame(table(nksnp_data[,choosen_idx[n]])) # Tally on # of 0,1,2
+  NKG7_nm<-NKG7[which(as.numeric(nksnp_data[,choosen_idx[n]])==2)]
+  RBC_nm<-RBC[which(as.numeric(nksnp_data[,choosen_idx[n]])==2)]
   variance_2<-sqrt(var(NKG7_nm)+var(RBC_nm))
   
   
   
-  if (variance_2 < 1.5 && !is.na(variance_2) ) ## a[3,2] >10
+  if (variance_2 < 2 && !is.na(variance_2) ) ## a[3,2] >10
   {
     
     variance_2
     quartz(width=3,height=6.5)
-    #print(variance_2)
-    #print(a[3,2])
     t[i]<-nktop_snps_symbol[n]
-    t0<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,chosen_idx1[n]]=='0',"#86B875",rgb(0.75, 0.75, 0.75,alpha=.5)),size=3)+labs(title=nktop_snps_symbol[chosen_idx1[n]])
-    t1<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,chosen_idx1[n]]=='1','#4CB9CC','gray'),size=3) 
-    t2<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,chosen_idx1[n]]=='2','#CD99D8',rgb(0.75, 0.75, 0.75,alpha=.5)),size=3)
+    t0<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,choosen_idx[n]]=='0',"#86B875",rgb(0.75, 0.75, 0.75,alpha=.5)),size=3)+labs(title=nktop_snps_symbol[choosen_idx[n]])
+    t1<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,choosen_idx[n]]=='1','#4CB9CC','gray'),size=3) 
+    t2<-qplot(NKG7,RBC) + geom_point(color=ifelse(nksnp_data[,choosen_idx[n]]=='2','#CD99D8',rgb(0.75, 0.75, 0.75,alpha=.5)),size=3)
     multiplot(t0,t1,t2)
     remove(t0,t1,t2)
     i=i+1
@@ -42,15 +40,15 @@ for(n in 1:length(chosen_idx1))
 #c<-numeric(0)
 
 
-#for(n in 1:length(chosen_idx1))
+#for(n in 1:length(choosen_idx))
 #{
 
 #a<-as.data.frame(table(nksnp_data[,chooosen_idx[n]])
 
 #b[n]<-a[3,2]
 #t[n]<-n
-#NKG7_nm<-NKG7[which(as.numeric(nksnp_data[,chosen_idx1[n]])==2)]
-#RBC_nm<-RBC[which(as.numeric(nksnp_data[,chosen_idx1[n]])==2)]
+#NKG7_nm<-NKG7[which(as.numeric(nksnp_data[,choosen_idx[n]])==2)]
+#RBC_nm<-RBC[which(as.numeric(nksnp_data[,choosen_idx[n]])==2)]
 #variance_2<-sqrt(var(NKG7_nm)+var(RBC_nm))
 #c[n]<-variance_2
 #remove(RBC_nm)
@@ -60,5 +58,6 @@ for(n in 1:length(chosen_idx1))
 
 
 #table(b>10)
+
 
 
